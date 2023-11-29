74fd7b31269ed97c4971b7003cdc623dde13aaee
JoaquinPan <105293020+JoaquinPan@users.noreply.github.com>
2023-11-30 00:09:51 +0800

    Add files via upload

---
+++ 成績計算網站/app.js
@@ -0,0 +1,192 @@
+// let hero = document.querySelector(".hero");
+// let slider = document.querySelector(".slider");
+// let animation = document.querySelector("section.animation-wrapper");
+
+// const time_line = new TimelineMax();
+
+// // parameter1 是要控制得對象
+// // parameter2 是duration
+// // parameter3 是控制對象得原始狀態
+// // parameter4 是控制對象得動畫結束後的狀態
+// // parameter5
+// time_line
+//   .fromTo(hero, 1, { height: "0%" }, { height: "100%", ease: Power2.easeInOut })
+//   .fromTo(
+//     hero,
+//     1.2,
+//     { width: "80%" },
+//     { width: "100%", ease: Power2.easeInOut }
+//   )
+//   .fromTo(
+//     slider,
+//     1,
+//     { x: "-100%" },
+//     { x: "0%", ease: Power2.easeInOut },
+//     "-=1.2"
+//   )
+//   .fromTo(animation, 0.3, { opacity: 1 }, { opacity: 0 });
+
+// // 設定timeout按鈕才能點
+// setTimeout(() => {
+//   animation.style.pointerEvents = "none";
+// }, 2500);
+
+// 讓整個網站的Enter key都無法使用
+window.addEventListener("keypress", (e) => {
+  if (e.key == "Enter") {
+    e.preventDefault();
+  }
+});
+
+// 防止Form內部的Button交出表單
+let allButtons = document.querySelectorAll("button");
+allButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+});
+
+// 選擇Select內的Option之後，要改變相應的顏色
+let allSelects = document.querySelectorAll("select");
+allSelects.forEach((select) => {
+  select.addEventListener("change", (e) => {
+    setGPA();
+    changeColor(e.target); // e.target就是<select>
+  });
+});
+
+// after change credit, GPA also have to change
+let credits = document.querySelectorAll(".class-credit");
+credits.forEach((credit) => {
+  credit.addEventListener("change", () => {
+    setGPA();
+  });
+});
+
+function changeColor(target) {
+  if (target.value == "A" || target.value == "A-") {
+    target.style.backgroundColor = "lightgreen";
+    target.style.color = "black";
+  } else if (
+    target.value == "B" ||
+    target.value == "B-" ||
+    target.value == "B+"
+  ) {
+    target.style.backgroundColor = "yellow";
+    target.style.color = "black";
+  } else if (
+    target.value == "C" ||
+    target.value == "C-" ||
+    target.value == "C+"
+  ) {
+    target.style.backgroundColor = "orange";
+    target.style.color = "black";
+  } else if (
+    target.value == "D" ||
+    target.value == "D-" ||
+    target.value == "D+"
+  ) {
+    target.style.backgroundColor = "red";
+    target.style.color = "black";
+  } else if (
+    target.value == "F" ||
+    target.value == "F-" ||
+    target.value == "F+"
+  ) {
+    target.style.backgroundColor = "grey";
+    target.style.color = "white";
+  } else {
+    target.style.backgroundColor = "white";
+  }
+}
+
+// convert grade to gpa
+function convertor(grade) {
+  switch (grade) {
+    case "A":
+      return 4.0;
+    case "A-":
+      return 3.7;
+    case "B+":
+      return 3.4;
+    case "B":
+      return 3.0;
+    case "B-":
+      return 2.7;
+    case "C+":
+      return 2.4;
+    case "C":
+      return 2.0;
+    case "C-":
+      return 1.7;
+    case "D+":
+      return 1.4;
+    case "D":
+      return 1.0;
+    case "D-":
+      return 0.7;
+    case "F":
+      return 0;
+    default:
+      return 0;
+  }
+}
+
+function setGPA() {
+  let formLength = document.querySelectorAll("form").length;
+  let credits = document.querySelectorAll(".class-credit");
+  let selects = document.querySelectorAll("select");
+  let sum = 0; //GPA計算分子
+  let creditSum = 0; //GPA計算分母
+
+  for (let i = 0; i < credits.length; i++) {
+    if (!isNaN(credits[i].valueAsNumber)) {
+      creditSum += credits[i].valueAsNumber;
+    }
+  }
+
+  for (let i = 0; i < formLength; i++) {
+    if (!isNaN(credits[i].valueAsNumber)) {
+      sum += credits[i].valueAsNumber * convertor(selects[i].value);
+    }
+  }
+
+  let result;
+  if (creditSum == 0) {
+    result = (0.0).toFixed(2);
+  } else {
+    result = (sum / creditSum).toFixed(2);
+  }
+  document.getElementById("result-gpa").innerText = result;
+}
+
+let addButton = document.querySelector(".plus-btn");
+addButton.addEventListener("click", () => {
+  let newForm = document.createElement("form");
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("grader");
+
+  // create 5 small element
+  let newInput1 = document.createElement("input");
+  newInput1.setAttribute("type", "text");
+  newInput1.setAttribute("list", "opt");
+  newInput1.classList.add("class-type");
+
+  let newInput2 = document.createElement("input");
+  newInput2.setAttribute("tupe", "text");
+  newInput2.classList.add("class-number");
+
+  let newInput3 = document.createElement("input");
+  newInput3.setAttribute("type", "number");
+  newInput3.setAttribute("min", "0");
+  newInput3.setAttribute("max", "6");
+  newInput3.classList.add("class-credit");
+  newInput3.addEventListener("change", () => {
+    setGPA();
+  });
+
+  newDiv.appendChild(newInput1);
+  newDiv.appendChild(newInput2);
+  newDiv.appendChild(newInput3);
+  document.querySelector(".all-inputs").appendChild(newForm);
+});
